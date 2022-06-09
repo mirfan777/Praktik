2000fd124b5f43e51026daefbb2a1287204afdd2
fcmirfan122 <fcmirfan122@gmail.com>
2022-06-09 20:57:29 +0700

    adding typescript

--- src/pages/hello.php
+++ src/pages/hello.php
@@ -9,12 +9,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="../css/style.css" rel="stylesheet">
+    <link href="../resource/css/style.css" rel="stylesheet">
     <title>Document</title>
 </head>
 <body>
-  <div class="text-3xl text-blue-900 font-bold flex justify-center items-center w-full h-20">
+  <div class="text-3xl text-red-900 font-bold flex flex-col justify-center items-center  h-56">
     <h1>IT'S WORKS!</h1>
+    <h1><?php echo $_ENV["text"] ?></h1>
   </div>
 </body>
 </html>
