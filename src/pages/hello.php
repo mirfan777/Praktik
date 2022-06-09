18887e0c55775ff6666d7075bde49492079c51b0
fcmirfan122 <fcmirfan122@gmail.com>
2022-06-09 17:01:05 +0700

    create environment

---
+++ src/pages/hello.php
@@ -0,0 +1,20 @@
+<!-- requirement -->
+<?php 
+    require '../../env.php'; 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../css/style.css" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+  <div class="text-3xl text-blue-900 font-bold flex justify-center items-center w-full h-20">
+    <h1>IT'S WORKS!</h1>
+  </div>
+</body>
+</html>
